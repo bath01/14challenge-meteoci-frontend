b64041b7dbbf8d9d09976861b5392a338b2d9988
alucard1702 <you@example.com>
2026-03-16 21:05:51 +0000

    titre

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  app: {
+  head: {
+    title: 'MétéoCI',
+    link: [
+      { rel: 'icon', href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>⛅</text></svg>' }
+    ]
+  }
+}
 })
